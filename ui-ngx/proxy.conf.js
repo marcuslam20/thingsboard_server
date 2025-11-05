4b5a9a808afb1c763dd8b3ee0a28fd1d1d191f98
marcuslam20 <lam.nguyen@dracaena.io>
2025-11-05 10:14:03 +0700

    Update signup page, signup with tenant ID

--- ui-ngx/proxy.conf.js
+++ ui-ngx/proxy.conf.js
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const forwardUrl = "http://192.168.68.128:8080";
-const wsForwardUrl = "ws://192.168.68.128:8080";
+const forwardUrl = "http://192.168.1.224:8080";
+const wsForwardUrl = "ws://192.168.1.224:8080";
 // const forwardUrl = "https://performentmarketing.ddnsgeek.com";
 // const wsForwardUrl = "https://performentmarketing.ddnsgeek.com";
 const ruleNodeUiforwardUrl = forwardUrl;
